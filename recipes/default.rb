d6ca38c89eefc039e1ce4ac8cadf47d5b3f180ad
chef <chef@chef.io>
2020-09-24 12:25:36 +0000

    change region

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,13 +4,13 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
-chef_gem "train" do
+chef_gem "inspec" do
   action :install
 end
 
 ENV['AWS_ACCESS_KEY_ID'] = "ASIAXHDZVFZT7BWNNUMM"
 ENV['AWS_SECRET_ACCESS_KEY_ID'] = "Xp/VFNnQtlHA0awEbKGQscY95sEk4c22jvc7Xbn1"
-ENV['AWS_REGION'] = "us-west-2"
+ENV['AWS_REGION'] = "ap-southeast-1"
 
 node.override['audit']['fetcher'] = 'chef-server'
 node.override['audit']['reporter'] = 'chef-server-automate'
@@ -19,10 +19,8 @@ node.override['audit']['reporter'] = 'chef-server-automate'
 case node['os']
 when 'linux'
   node.override['audit']['profiles']['aws-cloud']['compliance'] = "delivery/cis-aws-benchmark-level1"
-#  node.override['audit']['profiles']['aws-cloud2'] = {'url': 'https://github.com/inspec/inspec-aws/archive/v1.29.9.tar.gz'}
 when 'windows'
   node.override['audit']['profiles']['aws-cloud']['compliance'] = "delivery/cis-aws-benchmark-level1"
-#  node.override['audit']['profiles']['aws-cloud2']['compliance']['url'] = "https://github.com/inspec/inspec-aws/archive/v1.29.9.tar.gz"
 end
 
 include_recipe 'audit'
